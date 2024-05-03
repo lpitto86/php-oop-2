e1ffa47b13d5c449f7c806d55acf761cb4bddca7
lpitto86 <luca.p481@gmail.com>
2024-05-03 11:47:12 +0200

    Completo

--- index.php
+++ index.php
@@ -1,116 +1,124 @@
 <?php
-require_once __DIR__ . "/classes/product.php";
-require_once __DIR__ . "/product_subclasses/food.php";
-require_once __DIR__ . "/product_subclasses/game.php";
-require_once __DIR__ . "/product_subclasses/kennel.php";
-require_once __DIR__ . "/products.php";
+    require __DIR__.'/db/db.php';
+    require_once __DIR__.'/classes/product.php';
+    require_once __DIR__.'/classes/food.php';
+    require_once __DIR__.'/classes/dogbed.php';
+    require_once __DIR__.'/classes/toys.php';
+
+    // echo 'PRODOTTO SINGOLO';
+
+    $productOne = new product('Prodotto', 'https://www.my-personaltrainer.it/2023/11/28/gatto-tabby_900x760.jpeg', 20, 'Cane', 100, 'lorem ipsum', 3);
+    $products[] = $productOne;
+    // var_dump($productOne);
+
+    try{
+        $testProductCategory = new product('Prodotto', 'https://www.my-personaltrainer.it/2023/11/28/gatto-tabby_900x760.jpeg', 20, 'pappagallo', 100, 'lorem ipsum', 3);
+    } catch(Exception $e){
+        echo '<h3 class="fw-bold text-center my-3" style="color: red;">'.$e->getMessage().'</h3>';
+    }
+
+    try{
+        $testProductPrice = new product('Prodotto', 'https://www.my-personaltrainer.it/2023/11/28/gatto-tabby_900x760.jpeg', 'ciao', 'gatto', 100, 'lorem ipsum', 3);
+    } catch(Exception $e){
+        echo '<h3 class="fw-bold text-center my-3" style="color: red;">'.$e->getMessage().'</h3>';
+    }
+
+    try{
+        $testProductRating = new product('Prodotto', 'https://www.my-personaltrainer.it/2023/11/28/gatto-tabby_900x760.jpeg', 20, 'Cane', 100, 'lorem ipsum', -1);
+    } catch(Exception $e){
+        echo '<h3 class="fw-bold text-center my-3" style="color: red;">'.$e->getMessage().'</h3>';
+    }
+
+    // echo '<hr>';
+
+    // echo 'PRODOTTO CIBO';
+    $foodProductOne = new food('Cibo', 'https://www.ilpapaverorossoweb.it/sites/default/files/field/image/alimenti-per-cani.jpg', 30, 'Cane', 300, 'lorem ipsum', 2, '05/11/1999', 'lamponi', 36);
+    $products[] = $foodProductOne;
+    // var_dump($foodProductOne);
+    
+    // echo '<hr>';
+
+    // echo 'PRODOTTO CUCCIA';
+    $dogbedProcutOne = new dogbed('Cuccia', 'https://www.madaitalia.com/ecommerce/wp-content/uploads/2019/10/Vendita-Cuccia-Imbottita-Cani-Gatti-Sweet.jpg', 40, 'Gatto', 5000, 'lorem ipsum', 4, 'Micro pile', '30,5 X 21,7 cm');
+    $products[] = $dogbedProcutOne;
+    // var_dump($dogbedProcutOne);
+    
+    // echo '<hr>';
+
+    // echo 'PRODOTTO GIOCATTOLO';
+    $toyProcutOne = new toy('Giocattolo', 'https://www.biancheriaweb.it/wp-content/uploads/2024/02/GIOCO_OSSO_BLU.jpg', 15, 'Gatto', 372, 'lorem ipsum', 5, 'Plastica', 'false');
+    $products[] = $toyProcutOne;
+    // var_dump($toyProcutOne);
+
+    // echo'<hr>';
+
+    // echo 'ARRAY COMPLETO';
+    // var_dump($products);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Animal Shop</title>
 
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>PHP OOP 2</title>
-
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" />
-
-  <link rel="stylesheet" href="./css/style.css" />
-
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
-    integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
-    crossorigin="anonymous" referrerpolicy="no-referrer" />
-</head>
-
-<body>
-  <header>
-    <div class="container">
-      <h1>
-        DOG CAT WORLD
-      </h1>
-    </div>
-  </header>
-  <main>
-    <div class="container d-flex">
-      <?php
-      foreach ($all_products as $product) {
-        ?>
-        <div class="card ms-2 me-2" style="width: 18rem;">
-          <?php
-          if ($product->category->name == "dogs") {
-            ?>
-            <h3 class="text-end"><i class="fa-solid fa-dog"></i></h3>
-            <?php
-          } else if ($product->category->name == "cats") {
-            ?>
-              <h3 class="text-end"><i class="fa-solid fa-cat"></i></h3>
-          <?php } ?>
-          <img src=<?php echo $product->img ?> class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">
-              <?php echo $product->name ?>
-            </h5>
-            <p class="card-text">
-              <?php echo $product->description ?>
-            </p>
-            <h5>€
-              <?php echo $product->price ?>
-            </h5>
-            <?php
-            if (get_class($product) == "Food") {
-              ?>
-              <p>Ingredients:
-                <?php echo $product->ingredients ?>
-              </p>
-              <p>Proteins:
-                <?php echo $product->proteins ?>
-              </p>
-              <p>Fats:
-                <?php echo $product->fats ?>
-              </p>
-              <p>Calcium:
-                <?php echo $product->calcium ?>
-              </p>
-              <p>Phosphorus:
-                <?php echo $product->phosphorus ?>
-              </p>
-              <p>Magnesium:
-                <?php echo $product->magnesium ?>
-              </p>
-              <p>Humidity:
-                <?php echo $product->humidity ?>
-              </p>
-              <?php
-            }
-            ?>
-            <?php
-            if (get_class($product) == "Game") {
-              ?>
-              <p>Shape:
-                <?php echo $product->shape ?>
-              </p>
-              <p>Material:
-                <?php echo $product->material ?>
-              </p>
-            <?php } ?>
-            <?php
-            if (get_class($product) == "Kennel") {
-              ?>
-              <p>Size:
-                <?php echo $product->size ?>
-              </p>
-              <p>Softness:
-                <?php echo $product->softness ?>
-              </p>
-            <?php } ?>
-          </div>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    </head>
+    <body class="bg-dark">
+       <div class="container">
+            
+        <h1 class="text-center text-white my-4">Prodotti</h1>
+        
+        <div class="row">
+            <div class="col-12 d-flex">
+                <?php
+                    foreach($products as $singleProduct){
+                ?>
+                <div class="card col-3 mx-2" style="width: 18rem;">
+                    <img src="<?php echo $singleProduct->image?>" class="card-img-top h-100 object-fit-cover" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $singleProduct->name?></h5>
+                        <ul class="card-text list-unstyled p-0">
+                            <li>Prezzo: <?php echo $singleProduct->price?>€</li>
+                            <li>Categoria: <?php echo $singleProduct->category?></li>
+                            <li>Quantità: <?php echo $singleProduct->stock?></li>
+                            <li>Descrizione: <?php echo $singleProduct->description?></li>
+                            <li>Voti: <?php echo $singleProduct->ratings?></li>
+                            <li>Classe: <?php echo get_class($singleProduct)?></li>
+                            <?php
+                                if (get_class($singleProduct) == 'toy') {
+                            ?>
+                                <li>
+                                    Materiale: <?php echo $singleProduct->material; ?>
+                                </li>
+                            <?php
+                                }
+                                else if (get_class($singleProduct) == 'food') {
+                            ?>
+                                <li>
+                                    Data di scadenza: <?php echo $singleProduct->expireDate; ?>
+                                </li>
+                            <?php
+                                }
+                                else if (get_class($singleProduct) == 'dogbed'){
+                            ?>
+                                <li>
+                                    Dimensioni: <?php echo $singleProduct->size; ?>
+                                </li>
+                            <?php
+                                }
+                            ?>
+                        </ul>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                <?php
+                }
+                ?>
+            </div>
+            
         </div>
-        <?php
-      } ?>
-    </div>
-  </main>
-  <script type="text/javascript" src="./javascript/scripts.js"></script>
-</body>
-
+       </div> 
+    </body>
 </html>
---
+++ traits/HasMaterials.php
@@ -0,0 +1,5 @@
+<?php
+trait HasMaterial{
+    public $material;
+}
+?>
